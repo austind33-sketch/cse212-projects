bf5164f4799e8dfabb4818c835d3af597dfb2e6c
Austin Densley <austin.densley00@gmail.com>
2024-12-17 17:15:55 -0700

    W06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,8 +11,7 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
+        // Start Problem 1
         if (value < Data)
         {
             // Insert to the left
@@ -21,7 +20,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -29,17 +28,33 @@ public class Node
             else
                 Right.Insert(value);
         }
+        // If equal, do nothing (no duplicates allowed)
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
+        // Start Problem 2
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data && Left != null)
+        {
+            return Left.Contains(value);
+        }
+        else if (value > Data && Right != null)
+        {
+            return Right.Contains(value);
+        }
+
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = (Left == null) ? 0 : Left.GetHeight();
+        int rightHeight = (Right == null) ? 0 : Right.GetHeight();
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
